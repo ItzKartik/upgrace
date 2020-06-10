1b05cd88dbd4bfdd9b23d299da97781a4055cbc2
Prince Gupta <53069116+ItzKartik@users.noreply.github.com>
2020-06-10 21:08:42 +0530

    Update runme.py

--- insta_automation/runme.py
+++ insta_automation/runme.py
@@ -24,33 +24,33 @@ else:
         drivers.append(x)
         driver_urls.append(x.command_executor._url)
         session_ids.append(x.session_id)
-        x.get("https://instagram.com/instagram")
+        x.get("https://instagram.com")
+#         sleep(2)
+#         try:
+#             x.find_element_by_link_text("Follow").click()
+#         except (NoSuchElementException or ElementClickInterceptedException):
+#             sleep(1)
+#             x.find_element_by_link_text("Follow").click()
         sleep(2)
-        try:
-            x.find_element_by_link_text("Follow").click()
-        except (NoSuchElementException or ElementClickInterceptedException):
-            sleep(1)
-            x.find_element_by_link_text("Follow").click()
-        sleep(1)
         # email_input = x.find_element_by_name("username")
         # password_input = x.find_element_by_name("password")
         x.find_element_by_name("username").send_keys(i)
         x.find_element_by_name("password").send_keys("Kartik@777")
         sleep(2)
+#         try:
+#             x.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div/div/div[1]/div/form/div[4]/button') \
+#                 .click()
+#         except NoSuchElementException:
+#             x.find_element_by_xpath(
+#                 '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div') \
+#                 .click()
         try:
-            x.find_element_by_xpath('/html/body/div[5]/div[2]/div/div[2]/div/div/div[1]/div/form/div[4]/button') \
+            x.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button') \
                 .click()
         except NoSuchElementException:
             x.find_element_by_xpath(
-                '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div') \
+                '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button') \
                 .click()
-        # try:
-        #     x.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button') \
-        #         .click()
-        # except NoSuchElementException:
-        #     x.find_element_by_xpath(
-        #         '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button') \
-        #         .click()
         sleep(2)
         j = 0
         try:
